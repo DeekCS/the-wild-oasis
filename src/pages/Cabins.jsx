85a79b96468e0bec8eb87d9d5a33c01aea0ae771
deekCS <31720001079@std.bau.edu.jo>
2024-01-08 15:58:12 +0300

    Add React Query and React Query Devtools

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,29 +1,27 @@
-import { useEffect, useState } from "react";
+import CabinTable from "../features/cabins/CabinTable";
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
-  const [cabins, setCabins] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       const cabins = await getCabins();
       console.log(cabins);
-      setCabins(cabins);
     };
     fetchData();
   }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <ul>
-        {cabins.map((cabin) => (
-          <li key={cabin.id}>
-            {cabin.name} - {cabin.description}
-          </li>
-        ))}
-      </ul>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
